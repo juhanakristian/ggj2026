41548d4625b58a13e1c8f2ab816eed4059400430
w4zdev <markus.liuska@gmail.com>
2026-01-31 13:12:33 +0200

    Added audio stream player for intro music and fadeout + Audio busses

--- mauri/scenes/main/src/main.gd
+++ mauri/scenes/main/src/main.gd
@@ -3,12 +3,15 @@ class_name Main extends Node
 enum State {INIT, INTRO, WAIT, GAME } 
 var state : State = State.INIT
 
-
 @onready var game: Game = $Game
+@onready var intro_music_player: AudioStreamPlayer3D = $IntroMusicPlayer
+
 @export var curtain_operator : CurtainOperator
+
 
 func _ready() -> void:
 	curtain_operator.fade_dark()
+
 	
 func _process(delta: float) -> void:
 	match state:
@@ -34,7 +37,10 @@ func proc_intro(_delta : float) -> void:
 func proc_wait_state(_delta) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
 		print("Space was pressed, activating the game state")
+		var tween = get_tree().create_tween()
+		tween.tween_property(intro_music_player, "volume_db", -80.0, 1.0) 
 		await curtain_operator.fade_out().curtain_faded_out
+		
 		state = State.GAME
 		game.start_game()		
 
